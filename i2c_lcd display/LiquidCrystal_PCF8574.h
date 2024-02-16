b56f48eec1ac8eff2151d4cad6b14041ac0f16a4
AravindPosina <137607167+AravindPosina@users.noreply.github.com>
2024-02-16 14:39:53 +0530

    Add files via upload

---
+++ i2c_lcd display/LiquidCrystal_PCF8574.h
@@ -0,0 +1,8 @@
+#ifndef __LiquidCrystal_PCF8574__h
+#define __LiquidCrystal_PCF8574__h
+//#include "mraa/i2c.h"
+void lcd_init (void);
+void lcd_send_string (char *str);
+void setCursor(int a, int b);
+void lcd_clear (void);
+#endif
